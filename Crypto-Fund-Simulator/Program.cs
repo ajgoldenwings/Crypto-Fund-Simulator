94ea101ec7d36a8b260e5b789ac1996d8f461f6e
Anthony Pearson <ajgoldenwings@gmail.com>
2021-11-21 16:54:49 -0600

    Initial Commit

---
+++ Crypto-Fund-Simulator/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Crypto Fund Simulator");
+
+var minerCount = 0;
+var minerAverageCost = 2000;
+var minerAverageProfitabilityPerDay = 5;
+
+int tempTodaysRevenue;
+
+var currentCapital = 100000.0;
+var daysPerNextMinerPurchase = 1;
+
+var simulationDays = 1000;
+
+for (var currentDay = 0; currentDay < simulationDays; currentDay++)
+{
+    // Purchase Computer
+    if (currentDay % daysPerNextMinerPurchase == 0)
+    {
+        minerCount++;
+        currentCapital -= minerAverageCost;
+    }
+
+    // Calculate Todays Profit
+    tempTodaysRevenue = minerCount * minerAverageProfitabilityPerDay;
+
+    // Add Profit to Capital
+    currentCapital += tempTodaysRevenue;
+
+    Console.WriteLine($"Day: {currentDay}, Miner Count: {minerCount}, Todays Profit: {tempTodaysRevenue}, Current Capital: {currentCapital}");
+}
